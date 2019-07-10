d6fb0ffd0bc069fac45ff7eaa44023674e45053c
Alexey <170091@edu.psuti.ru>
2019-07-10 23:52:33 +0400

    Enum fixed

--- src/main/java/su/opencode/project/web/project/persistence/model/JobTitle.java
+++ src/main/java/su/opencode/project/web/project/persistence/model/JobTitle.java
@@ -1,7 +1,18 @@
 package su.opencode.project.web.project.persistence.model;
 
 public enum JobTitle {
-    ProjectManager,
-    Developer,
-    Designer
+    PROJECT_MANAGER("Менеджер проекта"),
+    DEVELOPER("Разработчик"),
+    DESIGNER("Дизайнер");
+
+    private String title;
+
+    JobTitle(String title){
+        this.title=title;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
 }
